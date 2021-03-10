c3f2c609562f3016dcf06c0330d66deb73f082b3
Ilan Cosman <ilancosman@gmail.com>
2021-03-10 09:46:57 -0800

    Install dependencies automatically

--- conf.d/dotfiles_events.fish
+++ conf.d/dotfiles_events.fish
@@ -1,4 +1,22 @@
 function dotfiles_install --on-event dotfiles_events_install
+    # Install dependencies if necessary
+    set -l installList
+    set -l dependencies bat fd fzf lsd
+    for dependency in $dependencies
+        type $dependency || set -a installList $dependency
+    end
+
+    if test -n "$installList"
+        type --query pacman && set -l packageManagerCommand sudo pacman -Syu
+        type --query brew && set -l packageManagerCommand brew install
+
+        set_color yellow
+        echo Installing (string join ', ' $installList)
+        set_color normal
+
+        $packageManagerCommand $installList
+    end
+
     # Plugins
     fisher install \
         jorgebucaran/autopair.fish \
