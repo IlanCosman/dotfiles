10b47e84e5d822244f425a9b946122b51f46f7f3
Ilan Cosman <ilancosman@gmail.com>
2023-04-19 13:46:49 -0700

    work

--- conf.d/dotfiles_events.fish
+++ conf.d/dotfiles_events.fish
@@ -1,30 +1,35 @@
 function dotfiles_install --on-event dotfiles_events_install
-    # Install dependencies if necessary
-    set -l installList
-    set -l dependencies bat fd fzf lsd
-    for dependency in $dependencies
-        type $dependency || set -a installList $dependency
+    # Install fisher if necessary
+    type --query fisher ||
+        curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish |
+            source && fisher install jorgebucaran/fisher
+
+    # fish_add_path /opt/homebrew/bin/ if it exists
+    if test -e /opt/homebrew/bin/
+        fish_add_path /opt/homebrew/bin/
     end
 
-    if test -n "$installList"
-        type --query pacman && set -l packageManagerCommand sudo pacman -Syu
-        type --query brew && set -l packageManagerCommand brew install
+    # Install dependencies if necessary
+    set -l install_list
+    set -l dependencies bat fd fzf lsd
+    for dependency in $dependencies
+        type --query $dependency || set -a install_list $dependency
+    end
+
+    if test -n "$install_list"
+        type --query pacman && set -l package_manager_install sudo pacman -Syu
+        type --query brew && set -l package_manager_install brew install
 
         set_color yellow
-        echo Installing (string join ', ' $installList)
+        echo Installing (string join ', ' $install_list)
         set_color normal
 
-        $packageManagerCommand $installList
+        $package_manager_install $install_list
     end
 
     # Plugins
     fisher install \
         jorgebucaran/autopair.fish \
         patrickf1/fzf.fish \
-        ilancosman/tide
-
-    # Abbrs
-    abbr gaa "git add -A"
-    abbr gca "git commit -am"
-    abbr gs "git status"
+        ilancosman/tide@v5
 end
